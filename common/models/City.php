4b386bedaa1ab060a9903115130726e1c77303de
Tu Beo <doanhtu278@gmail.com>
2015-05-07 12:42:20 +0700

    chuyển model
    chuyển city, district, ward sang common

---
+++ common/models/City.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "city".
+ *
+ * @property string $id
+ * @property string $name
+ *
+ * @property District[] $districts
+ */
+class City extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'city';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getDistricts()
+    {
+        return $this->hasMany(District::className(), ['city_id' => 'id']);
+    }
+
+    //function get data from City Table
+    public static function getCity(){
+        $data = static::find()->all();
+        $value = (count($data) == 0)? ['' => '']:\yii\helpers\ArrayHelper::map($data, 'id', 'name');
+
+        return $value;
+    }
+}
